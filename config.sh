a0dfdd08ac0e1b1b3571adcfda32e4a7be24c6fd
tmh1999 <tmhoang1999@gmail.com>
2016-06-26 05:07:11 +0700

    No need $ nproc . Try newer kernel header.

--- config.sh
+++ config.sh
@@ -21,14 +21,14 @@ CC_BASE_PREFIX=/opt/cross
 #GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
 #GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
 
-MAKEFLAGS=-j$(nproc)
+MAKEFLAGS=-j$(cat /proc/cpuinfo | grep processor | wc -l)
 
 # Enable this to build the bootstrap gcc (thrown away) without optimization, to reduce build time
 GCC_STAGE1_NOOPT=1
 
 if [ "$ARCH" = "s390x" ]
 then
-    LINUX_HEADERS_URL=http://www.kernel.org/pub/linux/kernel/v3.0/linux-3.12.6.tar.xz
+    LINUX_HEADERS_URL=https://cdn.kernel.org/pub/linux/kernel/v3.x/linux-3.18.36.tar.xz
     MUSL_GIT_REPO='https://github.com/koorogi/musl'
     MUSL_VERSION=s390x
     MUSL_GIT=yes
